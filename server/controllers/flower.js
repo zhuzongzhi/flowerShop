7945364dd1406799df70467d082ba6c0c646f65a
windmill <1031199762@qq.com>
2018-08-08 16:36:30 +0800

    modify

--- server/controllers/flower.js
+++ server/controllers/flower.js
@@ -34,13 +34,67 @@ async function queryFlowers (ctx, next) {
 
 // 新增修改花卉
 async function updateSertFlower (ctx, next) {
+    try {
+        const { flower_id } = ctx.request.body;
+        if (!flower_id) {
+            throw new Error('参数校验失败');
+        } else {
+            let flowerInfo = await mysql('flower').select('flower.*').where('id', flower_id);
 
+            if (!flowerInfo || flowerInfo.length === 0) {
+                await mysql('flower').insert({
+                    flower_name, flower_type_id, flower_language, flower_origina_price, 
+                    flower_current_price, is_new_product, sales_volume, deleted, create_id, create_time, 
+                    update_id, update_time
+                })
+            
+                ctx.state.data = {
+                    msg: '新增成功'
+                };
+            } else {
+                await mysql('flower').where(id, flower_id).update({
+                    flower_name
+                })
+            
+                ctx.state.data = {
+                    msg: '修改成功'
+                };
+            }
+        }
+    } catch (e) {
+        ctx.state = {
+            code: -1,
+            data: {
+                msg: `新增或修改失败:${e.message}`
+            }
+        }
+    }
 }
 
-// 删除花卉
+// 批量删除花卉
 async function removeFlowers (ctx, next) {
+    try {
+        const { flower_ids } = ctx.request.body;
+        if (!flower_ids || flower_ids.length === 0) {
+            throw new Error('参数校验失败');
+        } else {
+            await mysql('flower').where('id', 'in', flower_ids.join(',')).update({
+                deleted: 1
+            })
 
-}
+            ctx.data = {
+                msg: '删除成功'
+            }
+        } 
+    }catch (e) {
+        ctx.state = {
+            code: -1,
+            data: {
+                msg: `新增或修改失败:${e.message}`
+            }
+        }
+    }
+} 
 
 module.exports = {
     queryFlowers,
