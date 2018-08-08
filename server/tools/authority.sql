a414e61aca66d7c4ead8932bf40d665e42f0402d
1031199762@qq.com <1031199762@qq.com>
2018-08-08 10:41:22 +0800

    add sql 脚本

---
+++ server/tools/authority.sql
@@ -0,0 +1,74 @@
+-- 权限管理表
+
+-- 组织表
+DROP TABLE IF EXISTS `organization`;
+
+CREATE TABLE `organization` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `address` varchar(100) NOT NULL,
+
+  `legal_person_name` varchar(20) NOT NULL,
+  `legal_person_tel` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- 角色表
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- 用户表：使用 csessionInfo 表，详情见cAuth.sql
+
+-- 用户角色表
+DROP TABLE IF EXISTS `userRole`;
+
+CREATE TABLE `userRole` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `open_id` varchar(100) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- 系统表
+DROP TABLE IF EXISTS `system`;
+
+CREATE TABLE `system` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `system_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- 菜单表
+DROP TABLE IF EXISTS `menu`;
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `menu_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- 菜单路由表
+DROP TABLE IF EXISTS `menuRouter`;
+
+CREATE TABLE `menuRouter` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `menurouter_name` varchar(100) NOT NULL,
+  `menurouter_path` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- 角色菜单表
+DROP TABLE IF EXISTS `roleMenu`;
+
+CREATE TABLE `roleMenu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
